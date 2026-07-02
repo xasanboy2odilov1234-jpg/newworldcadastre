3f51c95c6e09bd81d0cc402412291001723b83af
xasanboy2odilov1234-jpg <xasanboy2odilov1234@gmail.com>
2026-07-03 00:35:41 +0500

    Reorder components on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import Stats from "@/components/Stats";
-import Certificates from "@/components/Certificates";
 import Portfolio from "@/components/Portfolio";
+import Certificates from "@/components/Certificates";
 import Gallery from "@/components/Gallery";
 import Reviews from "@/components/Reviews";
-import ContactForm from "@/components/ContactForm";
 import FAQ from "@/components/FAQ";
+import ContactForm from "@/components/ContactForm";
 import Contacts from "@/components/Contacts";
 
 export default function Home() {
@@ -14,12 +14,29 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-white">
       <Navigation />
       <Hero />
+      
+      {/* 1. Цифры и факты */}
       <Stats />
-      <Certificates />
+      
+      {/* 2. Процесс работы (Закомментировано, чтобы не сломать сайт. Создадим файл следующим шагом!) */}
+      {/* <Process /> */}
+
+      {/* 3. Завершенные проекты */}
       <Portfolio />
+
+      {/* 4. Лицензии и сертификаты */}
+      <Certificates />
+
+      {/* 5. Сотрудники (Пока поставил Gallery на это место, позже заменим на новый блок Team) */}
       <Gallery />
+
+      {/* 6. Отзывы */}
       <Reviews />
+
+      {/* 7. Частые вопросы */}
       <FAQ />
+
+      {/* 8. Контакты (Сначала форма связи, затем сами контактные данные) */}
       <ContactForm />
       <Contacts />
     </main>
